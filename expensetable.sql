20fa4ddd21b8db6bfcb13d7d33636949c4ce7879
yatw <yatw@uci.edu>
2018-05-09 19:42:33 -0700

    added SQL database

---
+++ expensetable.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS expensedb;
+CREATE DATABASE expensedb;
+USE expensedb;
+
+CREATE TABLE expense( 
+	id int NOT NULL AUTO_INCREMENT,
+    e_value float NOT NULL,
+    e_date date NOT NULL,
+    e_reason varchar(255) NOT NULL,
+    PRIMARY KEY (expenseID)
+);
+
+
